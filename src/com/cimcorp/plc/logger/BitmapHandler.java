754387f0952c384c68d917c3c7c682fb92e4f899
rbut <rbutler81@pm.me>
2021-03-26 14:31:55 -0400

    fixed warnings

--- src/com/cimcorp/plc/logger/BitmapHandler.java
+++ src/com/cimcorp/plc/logger/BitmapHandler.java
@@ -4,7 +4,6 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
